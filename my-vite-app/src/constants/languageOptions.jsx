de2ef89607e5510518340377982dff61b3a97d8b
Ely Cohen <elyanu@live.com>
2024-02-28 11:49:56 -0500

    added in tutorial IDE code

---
+++ my-vite-app/src/constants/languageOptions.jsx
@@ -0,0 +1,14 @@
+export const languageOptions = [
+    {
+      id: 70,
+      name: "Python (2.7.17)",
+      label: "Python (2.7.17)",
+      value: "python",
+    },
+    {
+      id: 71,
+      name: "Python (3.8.1)",
+      label: "Python (3.8.1)",
+      value: "python",
+    },
+  ];
