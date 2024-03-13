d064d72682ab0dbba335c376e1cc7e9813d2e531
joseph16 <joseph16@terpmail.umd.edu>
2024-03-13 18:33:21 -0400

    Delete app.py

--- group02/app.py
+++
@@ -1,8 +0,0 @@
-from api import fetch_semantic_scholar_data, display_author_data
-
-user_input = input("Enter a name: ")
-
-data = fetch_semantic_scholar_data(user_input)
-if data:
-    # print(data)
-    display_author_data(data)
