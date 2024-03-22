6b63a1e76fd927dcb18cffe7448c2ee816dbf8eb
prc <prc@terpmail.umd.edu>
2024-03-22 16:45:11 -0400

    Update 10 files
    - /data_storage_branch/new_graphclass/GraphClass/Tester/Paul/.gitkeep
    - /data_storage_branch/new_graphclass/GraphClass/Tester/Paul/test.csv
    - /data_storage_branch/new_graphclass/GraphClass/Tester/Paul/test2.csv
    - /data_storage_branch/new_graphclass/CSV_Parsing/parse.py
    - /data_storage_branch/new_graphclass/CSV_Parsing/Tester/test1-load.csv
    - /data_storage_branch/new_graphclass/CSV_Parsing/Tester/test1-save.csv
    - /data_storage_branch/new_graphclass/CSV_Parsing/Tester/test2-load.csv
    - /data_storage_branch/new_graphclass/CSV_Parsing/Tester/test2-save.csv
    - /data_storage_branch/new_graphclass/CSV_Parsing/Tester/test3-load.csv
    - /data_storage_branch/new_graphclass/CSV_Parsing/Tester/test3-save.csv

--- data_storage_branch/new_graphclass/CSV_Parsing/parse.py
+++ data_storage_branch/new_graphclass/CSV_Parsing/parse.py
@@ -1,8 +1,10 @@
 import dask.dataframe as dd
+import pandas
 
 def parseData(csv):
-    df = dd.read_csv(csv).compute()
-    cols = list()
+    df = dd.read_csv(csv)
+    df = df.where(df.notnull(), None).compute()
+
     offset = 0
 
     one = df.get(df.columns[0]).tolist()
@@ -16,15 +18,45 @@ def parseData(csv):
     values = df.get(df.columns[2 + offset]).tolist()
     for i in range(len(keys)):
         pairing = dict()
-        pairing[keys[i]] = values[i]
+        currkey = keys[i].split(',')
+        currvalue = values[i].split(',')
+        for x in range(len(currkey)):
+            pairing[currkey[x]] = currvalue[x]
         three.append(pairing)
 
     return (one, two, three)
 
-def saveData(csv):
-    pass
+# To do: need to rework to work with the correct data being passed in
+def saveData(filePath, values):
+
+    data = {'Person 1': values[0]}
+    if(values[1] != None):
+        data['Person 2'] = values[1]
+
+    ks = list()
+    vs = list()
+    for x in values[2]:
+        ks.append(','.join(list(x)))
+        vs.append(','.join(list(x.values())))
+
+    data['Relationship'] = ks
+    data['Relationship Value'] = vs
+
+    pandas_df = pandas.DataFrame(data)
+    df = dd.from_pandas(pandas_df, npartitions=1)
+    df.compute().to_csv(filePath, index=False)
+
+# temp testing
+test1 = parseData('data_storage_branch/new_graphclass/CSV_Parsing/Tester/test1-load.csv')
+test2 = parseData('data_storage_branch/new_graphclass/CSV_Parsing/Tester/test2-load.csv')
+test3 = parseData('data_storage_branch/new_graphclass/CSV_Parsing/Tester/test3-load.csv')
 
 
-# temp test prints
-print(parseData('new_connections/data_storage_branch/new_graphclass/GraphClass/Tester/Paul/test2.csv'))
-print(parseData('new_connections/data_storage_branch/new_graphclass/GraphClass/Tester/Paul/test.csv'))
+#print(test1)
+#print(test2)
+#print(test3)
+
+
+saveData('data_storage_branch/new_graphclass/CSV_Parsing/Tester/test1-save.csv', test1)
+saveData('data_storage_branch/new_graphclass/CSV_Parsing/Tester/test2-save.csv', test2)
+saveData('data_storage_branch/new_graphclass/CSV_Parsing/Tester/test3-save.csv', test3)
