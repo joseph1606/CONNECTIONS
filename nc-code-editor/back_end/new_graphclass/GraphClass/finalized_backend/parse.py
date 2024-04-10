86f0a2783f88c62281f774406bd5bbe9378c4a58
prc <prc@terpmail.umd.edu>
2024-04-10 17:12:37 -0400

    Added save feature to finalized backend and added tests to test.py

--- nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/parse.py
+++ nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/parse.py
@@ -1,25 +1,23 @@
 import dask.dataframe as dd
+import pandas
+
 
 """
-Mine (Joel's) comments
+Pauls comments
 
+To do after CDR
 i) Needs to be able to handle leading whitespace
-ii) DIRECTED needs to returns a tuple like (Mentor,Mentee) not a single string lile "Mentor/Mentee"
-iii) New change -> relationship inputs (like college) should be in CAPS
-iv) The attribute values for a DIRECTED type need to be in lowercase
-iv) there are some cases with comma delimited string where the parser also returns a few extra quotation marks
-v) DIRECTED inside a comma delimited string like "age,DIRECTED" works fine.
-    However, there is still the issue with quotation marks 
-vi) Maybe also need to test to make sure DIRECTED isnt inside type1 csv
+ii) Needs to be able to save graphs with directed edges
+    - Depending on implementation, throwing an error if directed is in a type1 csv.
+"""
 
-e.g. csv file
-Person1, Person2, Relationship, Relationship Value
-Joel,Ely,"sex,age","male,21"
-rev,paul,college,umd
-ely,purtilo,"age,DIRECTED","21,"Mentor/Mentee""
+"""
+Joels Comments
+iv) The attribute values for a DIRECTED type need to be in lowercase
 """
 
 
+# to do: leading whitespace cannot be read by dask, must look into this more
 def parseData(csv):
     df = dd.read_csv(csv).compute()
 
@@ -71,7 +69,7 @@ def parseData(csv):
             
         # Loops through all relationships given in current cell
         for x in range(len(currkey)):
-            key = currkey[x].strip()
+            key = currkey[x].strip().title()
             if key.isspace() or not key: #if the key is empty or whitespace an error occurs
                 raise ValueError("Error: All relationships must have corresponding value")
             
@@ -91,7 +89,8 @@ def parseData(csv):
                 if (not rel[0] or not rel[1]):
                     raise ValueError("Error: Incorrect directed relationship format")
                 value = (rel[0], rel[1])
-            
+            else:
+                value = value.title()
             # Saves the key,value pair to the dictionary
             if key in pairing:
                 pairing[key].append(value)  # If key already exists, append value to existing list
@@ -101,3 +100,43 @@ def parseData(csv):
         three.append(pairing)
 
     return (one, two, three)
+
+# to do: cannot save graphs with directed edges
+def saveData(nodes, filePath):
+    names = list()
+    attributes = list()
+
+    # Gets all information out of the list of nodes
+    for node in nodes:
+        names.append(node.getName())
+        attributes.append(node.getAttributes())
+
+    # Formats data in the correct way
+    data = dict()
+    ks = list()
+    vs = list()
+    for x in attributes:
+        keyslist = list(x.keys())
+        valuelist = list(x.values())
+        values_to_save = list()
+        keys_to_save = list()
+
+        # Unpacks list of lists into values
+        for y in range(len(valuelist)):
+            for z in range(len(valuelist[y])):
+                values_to_save.append(valuelist[y][z])
+                keys_to_save.append(keyslist[y])
+
+        vs.append(','.join(values_to_save))
+        ks.append(','.join(keys_to_save))
+
+
+    # Puts all data into a dictionary
+    data["Person 1"] = names
+    data['Relationship'] = ks
+    data['Relationship Value'] = vs
+
+    # Saves dictionary to csv
+    pandas_df = pandas.DataFrame(data)
+    df = dd.from_pandas(pandas_df, npartitions=1)
+    df.compute().to_csv(filePath, index=False)
--- nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/test.py
+++ nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/test.py
@@ -1,8 +1,84 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-# from Functions import *
+from Functions import *
 from parse import parseData
 from SemanticScholarFuncs import *
+
+
+x = CreateGraph("new_connections-Group2Backend/nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/connections3.csv")
+y = CreateGraph("new_connections-Group2Backend/nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/connections.csv")
+z = MergeGraph(x,y)
+
+saveData(x.get_nodes(), "new_connections-Group2Backend/nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/test1.csv")
+saveData(y.get_nodes(), "new_connections-Group2Backend/nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/test2.csv")
+saveData(z.get_nodes(), "new_connections-Group2Backend/nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/test3.csv")
+
+a = CreateGraph("new_connections-Group2Backend/nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/test1.csv")
+b = CreateGraph("new_connections-Group2Backend/nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/test2.csv")
+c = CreateGraph("new_connections-Group2Backend/nc-code-editor/back_end/new_graphclass/GraphClass/finalized_backend/test3.csv")
+
+# a = x, b = y, c = z
+#visualization seems to be broken, temp test code is commented below
+
+
+
+
+
+"""
+a.print_nodes()
+print("``````````")
+x.print_nodes()
+print("~~~~~~")
+a.print_edges()
+print("``````````")
+x.print_edges()
+print("~~~~~~")
+a.print_relationships()
+print("``````````")
+x.print_relationships()
+print("---------------------------------------------------------------------------")
+
+b.print_nodes()
+print("``````````")
+y.print_nodes()
+print("~~~~~~")
+b.print_edges()
+print("``````````")
+y.print_edges()
+print("~~~~~~")
+b.print_relationships()
+print("``````````")
+y.print_relationships()
+print("---------------------------------------------------------------------------")
+
+c.print_nodes()
+print("``````````")
+z.print_nodes()
+print("~~~~~~")
+c.print_edges()
+print("``````````")
+z.print_edges()
+print("~~~~~~")
+c.print_relationships()
+print("``````````")
+z.print_relationships()
+print("---------------------------------------------------------------------------")
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # x = CreateGraph("connections3.csv")
 # print("---------------------------------------------------------------------------")
 # y = CreateGraph("connections.csv")
@@ -23,8 +99,8 @@ from SemanticScholarFuncs import *
 #f.print_edges()
 #f.print_relationships()
 
-user_input = "James Purtilo"
-searchAuthor(user_input)
+#user_input = "James Purtilo"
+#searchAuthor(user_input)
 # x = makeAuthor(user_input,1,5)
 # print_author_details(x)
 # generate_author_dict(user_input,1)
