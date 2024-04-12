498ed2656692df02590abc069716da274d509d05
walterosborne <willow3085@gmail.com>
2024-04-11 21:37:10 -0400

    one line

--- nc-code-editor/src/components/REPL.js
+++ nc-code-editor/src/components/REPL.js
@@ -68,7 +68,6 @@ const REPL = () => {
     const handleFileUpload = async (event) => {
         const file = event.target.files[0];
         // WALTER: file.name will give you all the file names
-        // ANDREW: tanks that was helpful
         const formData = new FormData();
         formData.set('file', file);
         formData.set('csvName', file.name);
