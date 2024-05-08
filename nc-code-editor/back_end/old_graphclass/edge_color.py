9f8b6aaa95bcbc9832b4ed8ff10cab41ca05abd7
atimmer <atimmer@terpmail.umd.edu>
2024-05-07 21:52:12 -0400

    deleted unnecessary files

--- nc-code-editor/back_end/old_graphclass/edge_color.py
+++
@@ -1,5 +0,0 @@
-class EdgeColor:
-    """This class stores (R,G,B) values for the color of an edge. This class
-    is used when making an edge to denote it as a specific work. """
-    PUBLICATION = "#008000" # green
-    PATENT = "#FF0000" # red
